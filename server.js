695441d9d3628dbbba63fca333f51024c3b3aa9b
Brian Gerson <brianpgerson@gmail.com>
2018-01-24 12:22:17 -0800

    Add config file

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express'),
+	   config = require('./config/main.js'),
 		  app = express();
 
 require('dotenv').config();
@@ -6,4 +7,4 @@ require('dotenv').config();
 
 app.get('/', (req, res) => res.send('I am Pliny! I will help you find my tasty beer!'));
 
-app.listen(3000, () => console.log(`Example app listening on port ${process.env.PORT}!`));
+app.listen(3000, () => console.log(`Example app listening on port ${config.port}!`));
