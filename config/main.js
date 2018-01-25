d9b636e1309e44e7dbe12e26f6aeb5da09b5c237
Brian Gerson <brianpgerson@gmail.com>
2018-01-24 18:30:50 -0800

    Stream works

--- config/main.js
+++ config/main.js
@@ -1,3 +1,7 @@
 module.exports = {
-	port: process.env.PORT
+	port: process.env.PORT,
+	twitter_consumer_key: process.env.TWITTER_API_KEY,
+	twitter_consumer_secret: process.env.TWITTER_SECRET,
+	twitter_access_token: process.env.TWITTER_ACCESS_TOKEN,
+	twitter_access_token_secret: process.env.TWITTER_ACCESS_SECRET
 }
--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 require('dotenv').config();
 const express = require('express'),
-	   config = require('./config/main.js'),
+	   config = require('./config/main'),
+	  twitter = require('./lib/twitter/twitter.service'),
 		  app = express();
 
 app.get('/', (req, res) => res.send('I am Pliny! I will help you find my tasty beer!'));
 
 app.listen(3000, () => console.log(`Example app listening on port ${config.port}!`));
+
+let stream = twitter.streamForPlinyBars();
+
+stream.on('tweet', (tweet) => {
+	let txt = tweet.text;
+	if (twitter.isLikelyTweet(txt)) {
+		console.log(`Pliny-related tweet from ${tweet.user.screen_name}: ${txt}`);
+	}
+});
