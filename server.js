2526d36d16ac84c5d1d1e6325acb65300727e129
Brian Gerson <brianpgerson@gmail.com>
2018-01-29 10:29:03 -0800

    Most recent stuff

--- server.js
+++ server.js
@@ -8,16 +8,17 @@ app.get('/', (req, res) => res.send('I am Pliny! I will help you find my tasty b
 
 app.listen(3000, () => console.log(`Example app listening on port ${config.port}!`));
 
-let barStream = twitter.streamForPlinyBars();
+// let barStream = twitter.streamForPlinyBars();
+// let plinyFinder = twitter.streamForPlinyFinder();
 let plinyStream = twitter.streamForBarMentions();
-let plinyFinder = twitter.streamForPlinyFinder();
 
-barStream.on('tweet', tweetAboutBarMatchesPlinyMention);
+// barStream.on('tweet', tweetAboutBarMatchesPlinyMention);
+// plinyFinder.on('tweet', tweetAboutPlinyMatchesBarName);
 plinyStream.on('tweet', tweetAboutPlinyMatchesBarName);
-plinyFinder.on('tweet', tweetAboutPlinyMatchesBarName);
 
 function tweetAboutPlinyMatchesBarName (tweet) {
 	let txt = tweet.text;
+	console.log('hmm, we have a tweet about a bar', tweet.text)
 	if (twitter.isLikelyViaBarMention(txt)) {
 
 		//TODO: slack hook
@@ -28,7 +29,6 @@ function tweetAboutPlinyMatchesBarName (tweet) {
 function tweetAboutBarMatchesPlinyMention (tweet) {
 	let txt = tweet.text;
 	if (twitter.isLikelyViaPlinyMention(txt)) {
-
 		//TODO: slack hook
 		console.log(`Pliny-related tweet from ${tweet.user.screen_name}: ${txt}`);
 	}
