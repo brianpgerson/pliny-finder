5cd68f8d2b023135713a3f11d266054beb18b5aa
Brian Gerson <brianpgerson@gmail.com>
2018-02-09 14:34:40 -0800

    got twilio workin

--- config/main.js
+++ config/main.js
@@ -3,5 +3,11 @@ module.exports = {
 	twitter_consumer_key: process.env.TWITTER_API_KEY,
 	twitter_consumer_secret: process.env.TWITTER_SECRET,
 	twitter_access_token: process.env.TWITTER_ACCESS_TOKEN,
-	twitter_access_token_secret: process.env.TWITTER_ACCESS_SECRET
+	twitter_access_token_secret: process.env.TWITTER_ACCESS_SECRET,
+	twilio: {
+		accountSid: process.env.ACCOUNT_SID,
+		authToken: process.env.AUTH_TOKEN,
+		fromNumber: process.env.FROM_NUMBER
+	}
+		
 }
--- server.js
+++ server.js
@@ -2,34 +2,43 @@ require('dotenv').config();
 const express = require('express'),
 	   config = require('./config/main'),
 	  twitter = require('./lib/twitter/twitter.service'),
+	  twilio = require('./lib/twilio/twilio.service'),
 		  app = express();
 
 app.get('/', (req, res) => res.send('I am Pliny! I will help you find my tasty beer!'));
 
 app.listen(3000, () => console.log(`Example app listening on port ${config.port}!`));
 
-// let barStream = twitter.streamForPlinyBars();
-// let plinyFinder = twitter.streamForPlinyFinder();
-let plinyStream = twitter.streamForBarMentions();
+twilio.test();
 
-// barStream.on('tweet', tweetAboutBarMatchesPlinyMention);
-// plinyFinder.on('tweet', tweetAboutPlinyMatchesBarName);
+// stream of bars that serve pliny in SF
+let barStream = twitter.streamForPlinyBars();
+
+// stream of tweets by a specific dude who does something useful
+let plinyFinder = twitter.streamForPlinyFinder();
+
+// stream of tweets mentioning pliny
+let plinyStream = twitter.streamForPlinyMentions();
+
+barStream.on('tweet', tweetFromBarMatchesPlinyMention);
+plinyFinder.on('tweet', tweetAboutPlinyMatchesBarName);
 plinyStream.on('tweet', tweetAboutPlinyMatchesBarName);
 
 function tweetAboutPlinyMatchesBarName (tweet) {
 	let txt = tweet.text;
-	console.log('hmm, we have a tweet about a bar', tweet.text)
+	console.log(`tweet about pliny the younger: ${txt}`)
 	if (twitter.isLikelyViaBarMention(txt)) {
-
-		//TODO: slack hook
-		console.log(`Pliny-related tweet from ${tweet.user.screen_name}: ${txt}`);
+		let msg = `Pliny-related tweet from ${tweet.user.screen_name}: ${txt}`;
+		twilio.notify(msg);
+		console.log(msg);
 	}
 }
 
-function tweetAboutBarMatchesPlinyMention (tweet) {
+function tweetFromBarMatchesPlinyMention (tweet) {
 	let txt = tweet.text;
 	if (twitter.isLikelyViaPlinyMention(txt)) {
-		//TODO: slack hook
-		console.log(`Pliny-related tweet from ${tweet.user.screen_name}: ${txt}`);
+		let msg = `Pliny-related tweet from ${tweet.user.screen_name}: ${txt}`;
+		twilio.notify(msg);
+		console.log(msg);
 	}
 }
